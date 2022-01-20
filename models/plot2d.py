327852272653b846820e7c0af53994d82a1e4e4c
LouisXVBTW <louisoregan.2001@gmail.com>
2022-01-20 22:42:54 +0000

    done for tonight

--- models/plot2d.py
+++ models/plot2d.py
@@ -17,15 +17,16 @@ print (data.shape)
 
 #print(data[:, 1])
 this = data[:]
-count = 0
-for level in range(0,9):
-    print('LEVEL ', level)
+print (this[6])
+##count = 0
+#for level in range(0,9):
+    #print('LEVEL ', level)
     #print (this[level])
-    for row in range(0,9):
-        print('ROW ', row)
+ #   for row in range(0,9):
+        #print('ROW ', row)
         #print (this[level][row])
-        for block in range(0,9):
-            if data[level][row][block] == True:
-                print(data[level][row][block])
-                count += 1
-print(count)
+  #      for block in range(0,9):
+   #         if data[level][row][block] == True:
+    #            print(data[level][row][block])
+     #           count += 1
+#print(count
